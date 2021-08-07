3b390ef41136f04f9786592b23c5f9d863b210e6
9033 <28668509+9033@users.noreply.github.com>
2021-08-07 15:49:13 +0900

    interface union structural type

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+"use strict";
+var account = {
+    name: "김밥",
+    id: 0,
+};
+console.log(account);
 function showPresidents(name) {
     console.log(name);
 }
-var presidents = "노무현";
-showPresidents(presidents);
+showPresidents("볶음밥");
+var best = "돈까스";
+var good = "알밥";
+var names = [best, good];
+showPresidents(names);
+function showPresident(name) {
+    console.log(name);
+    return name;
+}
+showPresident("돈까스");
+var logPoint = function (p) { return p.x + ", " + p.y; };
+var point = { x: 12, y: 26 };
+console.log(logPoint(point));
--- index.ts
+++ index.ts
@@ -1,9 +1,42 @@
-type presidents = "노무현" | "김대중" | "이명박"
+// interface (class, object)
+interface user {
+  name: string;
+  id: number;
+}
+
+const account: user = {
+  name: "김밥",
+  id: 0,
+};
+
+console.log(account);
+
+// union (enum)
+type presidents = "돈까스" | "알밥" | "볶음밥"
 
 function showPresidents(name: presidents | presidents[]){
   console.log(name)
 }
 
-const presidents = "노무현"
+showPresidents("볶음밥")
 
-showPresidents(presidents)
+const best: presidents = "돈까스"
+const good: presidents = "알밥"
+const names = [best, good]
+showPresidents(names)
+
+function showPresident(name: presidents): presidents{
+  console.log(name)
+  return name
+}
+showPresident("돈까스")
+
+// structural type system
+interface Point {
+  x: number;
+  y: number;
+}
+
+const logPoint = (p: Point) => `${p.x}, ${p.y}`
+const point = { x: 12, y:26 };
+console.log(logPoint(point))
